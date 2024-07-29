34f830fff79592d52c524026add2b181cd606197
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2024-07-29 13:21:20 -0300

    ADD Package Model + Classes

---
+++ lab-api-rest/src/main/java/me/aline/lab_api_rest/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package me.aline.lab_api_rest.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ lab-api-rest/src/main/java/me/aline/lab_api_rest/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.aline.lab_api_rest.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
